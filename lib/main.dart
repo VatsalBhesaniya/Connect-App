9272af25bf5e26eecd784452c5b565141d14f0cd
Vatsal Bhesaniya <bhesaniyavatsal@gmail.com>
2024-02-21 13:38:34 +0530

    Firebase config (#2)
    * remove firebasesrc file
    
    * added dotenv to store variables

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,18 @@ import 'package:connect/repository/authentication_repository.dart';
 import 'package:connect/repository/user_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyAMOBi-xg92fQxFQ5XHzyaYgSmq05ISAKI',
-      appId: '1:721996489807:web:49da3c79334e3ba202b63a',
-      messagingSenderId: '721996489807',
-      projectId: 'connect-prod-76d73',
+    options: FirebaseOptions(
+      apiKey: dotenv.env['API_KEY']!,
+      appId: dotenv.env['APP_ID']!,
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
+      projectId: dotenv.env['PROJECT_ID']!,
     ),
   );
   EasyLoading.instance
